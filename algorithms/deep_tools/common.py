71dceed3cf7e70edc6cebb1aba7bd018a301f0c8
luisferreira32 <luis.pmm.ferreira@gmail.com>
2020-11-20 20:24:51 +0000

    gae and basic advantage estimators

--- algorithms/deep_tools/common.py
+++ algorithms/deep_tools/common.py
@@ -1,18 +1,58 @@
 #!/usr/bin/env python
+import numpy as np
+import copy
+import tensorflow as tf
+from typing import Tuple
 
+# near 0 number
+eps = np.finfo(np.float32).eps.item()
 
-# TODO@luis: re-do this
 # General Advantage Estimator
-def get_gaes(rewards, dones, values, next_values, gamma, lamda, normalize):
-	deltas = [r + gamma * (1 - d) * nv - v for r, d, nv, v in zip(rewards, dones, next_values, values)]
+def get_gaes(rewards: tf.Tensor, values: tf.Tensor, next_values: tf.Tensor, lbd: float,
+	gamma: float, standardize: bool = True) -> Tuple[tf.Tensor, tf.Tensor]:
+	"""Compute the general advantage estimation per timestep
+	"""
+	deltas = [r + gamma * nv - v for r, nv, v in zip(rewards, next_values, values)]
 	deltas = np.stack(deltas)
 	gaes = copy.deepcopy(deltas)
 	for t in reversed(range(len(deltas) - 1)):
-    	gaes[t] = gaes[t] + (1 - dones[t]) * gamma * lamda * gaes[t + 1]
+		gaes[t] = gaes[t] + gamma * lbd * gaes[t + 1]
 
-	target = gaes + values
-	if normalize:
-    	gaes = (gaes - gaes.mean()) / (gaes.std() + 1e-8)
-	return gaes, target
+	returns = gaes + values
+	if standardize:
+		gaes = (gaes - gaes.mean()) / (gaes.std() + eps)
 
-# build a normal advantage calculation too (?)
+	# advantage and expected returns Q(s,a)
+	return tf.convert_to_tensor(gaes), tf.convert_to_tensor(returns)
+
+# Or just the basic
+def get_simple_advantage(rewards: tf.Tensor, values: tf.Tensor,
+	gamma: float, standardize: bool = True) -> tf.Tensor:
+	"""Compute expected returns per timestep with just discounted sum
+	G_t = sum(from t'=t to T){ gamma^(t'-t) * r_t'}
+	"""
+
+	n = tf.shape(rewards)[0]
+	returns = tf.TensorArray(dtype=tf.float32, size=n)
+
+	# Start from the end of `rewards` and accumulate reward sums
+	# into the `returns` array
+	rewards = tf.cast(rewards[::-1], dtype=tf.float32)
+	discounted_sum = tf.constant(0.0)
+	discounted_sum_shape = discounted_sum.shape
+	for i in tf.range(n):
+		reward = rewards[i]
+		discounted_sum = reward + gamma * discounted_sum
+		discounted_sum.set_shape(discounted_sum_shape)
+		returns = returns.write(i, discounted_sum)
+	returns = returns.stack()[::-1]
+
+	if standardize:
+		returns = ((returns - tf.math.reduce_mean(returns)) / (tf.math.reduce_std(returns) + eps))
+
+	# deal with multi-discrete action spaces
+	returns = tf.convert_to_tensor([returns for v in values[0]])
+	returns = tf.transpose(returns)
+
+	# advantage and expected returns Q(s,a)
+	return returns-values, returns
