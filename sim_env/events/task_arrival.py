eab99326faa48ade7028b38a119c3dab500c69ac
Luis Ferreira <luis.pires.de.matos@tecnico.ulisboa.pt>
2020-11-29 18:51:24 +0000

    unit testing and documant task_arrival.py

--- sim_env/events/task_arrival.py
+++ sim_env/events/task_arrival.py
@@ -1,28 +1,70 @@
 #!/usr/bin/env python
+"""Provides a class and auxiliary functions for the event responsible for making a task arrive at a certain node and slice"""
 
+# >>>>> imports
 from .core import Event
+from utils.custom_exceptions import InvalidValueError
 
+# <<<<<
+# >>>>> meta-data
+__author__ = "Luis Ferreira @ IST"
+
+# <<<<<
+# >>>>> class and functions
 class Task_arrival(Event):
-	""" Task_arrival inserts a task to a node slice, if it overflows returns the task
+	""" Task_arrival is an event to either insert the task to a node slice or return it in case of overflow.
+
+	Attributes:
+		(super) time: float - the current simulation time
+		node: Fog_nodes - an insntance of the class that represents a Fog node hardware strucutre
+		k: int - the slice in which the task is arriving
+		task: Task - the task that is arriving at the said slice and node
 	"""
 	def __init__(self, time, node, k, task):
+		"""
+		Parameters:
+			time: float - the current simulation time
+			node: Fog_nodes - an insntance of the class that represents a Fog node hardware strucutre
+			k: int - the slice in which the task is arriving
+			task: Task - the task that is arriving at the said slice and node
+		"""
+
 		super(Task_arrival, self).__init__(time, "Task_arrival")
 		self.node = node
 		self.k = k
 		self.task = task
-		assert time >= task.task_time()
+		# cannot recieve wrong slice
+		if k >= node.max_k or k < 0:
+			raise InvalidValueError("Invalid slice number", "[0,"+str(node.max_k)+"[")
+		# cannot recieve future tasks
+		if time < task.task_time():
+			raise InvalidValueError("Cannot recieve a task from the future")
 
 	def execute(self, evq):
-		# if the task just arrived, schedule a discard for it's deadline (in milliseconds) # NOTE: only when deciding for now
-		# evq.addEvent(Discard_task(self.task.task_time()+(0.001*self.task.delay_constraint), self.node, self.k, self.task))
-		return self.node.add_task_on_slice(self.k, self.task)
+		""" Executes a task arrival on a said node and slice, returning the task value if there was an overflow.
 
-	def task_time(self):
-		return self.task.task_time()
+		Parameters:
+			evq: Event_queue - the event queue from which this event was called and to which it can add events
+		"""
+		return self.node.add_task_on_slice(self.k, self.task)
 
 
 def is_arrival_on_slice(ev, node, k):
+	""" Small function to test if an event is an arrival on the node n and slice k
+
+	Parameters:
+		ev: Event - an event, subclass of Event(ABC)
+		node: Fog_node - a fog node class instanciation
+		k: int - a valid slice index/number
+	"""
 	return (ev.classtype == "Task_arrival" and ev.node == node and ev.k == k)
 
-def is_offload_arrival_event(ev, clock):
-	return (ev.classtype == "Task_arrival" and ev.task_time() < clock)
+def is_offload_arrival_event(ev):
+	""" Small function to test if an event is an arrival from an offloading event
+
+	Parameters:
+		ev: Event - an event, subclass of Event(ABC)
+	"""
+	return (ev.classtype == "Task_arrival" and ev.task.task_time() < ev.time)
+
+# <<<<<
