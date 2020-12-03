330f0385455346019a8cc66c58eb67ec9e41e83d
Luis Ferreira <luis.pires.de.matos@tecnico.ulisboa.pt>
2020-12-03 22:15:58 +0000

    reward calculation optimized

--- sim_env/environment.py
+++ sim_env/environment.py
@@ -255,36 +255,38 @@ class Fog_env(gym.Env):
 		[fks, wks] = np.split(action, 2)
 		concurr = sum([1 if fk!=n.index and fk!=0 else 0 for fk in fks])
 		for k in range(n.max_k):
-			D_ik = 0; Dt_ik = 0
-			# if it's offloaded adds communication time to delay
-			if fks[k] != n.index and fks[k] != 0:
-				bw = int(n.available_bandwidth()/concurr)
-				if bw >= NODE_BANDWIDTH_UNIT: # just make sure it is actually going to offload
-					Dt_ik = PACKET_SIZE / (point_to_point_transmission_rate(n._distances[fks[k]],concurr))
-					D_ik += Dt_ik*1000 # converto to milliseconds
-			# calculate the Queue delay: b_ik/service_rate_i
-			D_ik += obs[n.max_k+k]/n._service_rate[k] # service rate per millisecond
-			# and the processing delay T*slice_k_cpu_demand / CPU_UNIT (GHz)
-			D_ik +=  1000* PACKET_SIZE*self.case["task_type"][k][1] / (CPU_UNIT) # convert it to milliseconds
-			# finally, check if slice delay constraint is met
-			if D_ik >= self.case["task_type"][k][0]:
-				coeficient = -1
-			else:
-				coeficient = 1
+			# only makes sense to calculate the coefficient if a[k] = 1
+			if obs[k]:
+				D_ik = 0; Dt_ik = 0
+				# if it's offloaded adds communication time to delay
+				if fks[k] != n.index and fks[k] != 0:
+					bw = int(n.available_bandwidth()/concurr)
+					if bw >= NODE_BANDWIDTH_UNIT: # just make sure it is actually going to offload
+						Dt_ik = PACKET_SIZE / (point_to_point_transmission_rate(n._distances[fks[k]],concurr))
+						D_ik += Dt_ik*1000 # converto to milliseconds
+				# calculate the Queue delay: b_ik/service_rate_i
+				D_ik += obs[n.max_k+k]/n._service_rate[k] # service rate per millisecond
+				# and the processing delay T*slice_k_cpu_demand / CPU_UNIT (GHz)
+				D_ik +=  1000* PACKET_SIZE*self.case["task_type"][k][1] / (CPU_UNIT) # convert it to milliseconds
+				# finally, check if slice delay constraint is met
+				if D_ik >= self.case["task_type"][k][0]:
+					coeficient = -1
+				else:
+					coeficient = 1
 
-			# count the number of new arrivals in the arriving node
-			arr = 0
-			for ev in self.evq.queue():
-				if is_arrival_on_slice(ev, self.nodes[fks[k]-1], k) and ev.time <= self.clock+Dt_ik:
-					arr += 1
-			# also, verify if there is an overload chance in the arriving node
-			arrival_node = self.nodes[fks[k]-1] if fks[k] > 0 else n
-			arr_obs = self._get_agent_observation(arrival_node)
-			if arr_obs[DEFAULT_SLICES+k]+arr+1 >= MAX_QUEUE:
-				coeficient -= OVERLOAD_WEIGHT # tunable_weight
+				# count the number of new arrivals in the arriving node
+				arr = 0
+				for ev in self.evq.queue():
+					if is_arrival_on_slice(ev, self.nodes[fks[k]-1], k) and ev.time <= self.clock+Dt_ik:
+						arr += 1
+				# also, verify if there is an overload chance in the arriving node
+				arrival_node = self.nodes[fks[k]-1] if fks[k] > 0 else n
+				arr_obs = self._get_agent_observation(arrival_node)
+				if arr_obs[DEFAULT_SLICES+k]+arr+1 >= MAX_QUEUE:
+					coeficient -= OVERLOAD_WEIGHT # tunable_weight
 
-			# a_ik * ( (-1)if(delay_constraint_unmet) - (tunable_weight)if(overflow_chance) )
-			node_reward += obs[k] * coeficient
+				# a_ik * ( (-1)if(delay_constraint_unmet) - (tunable_weight)if(overflow_chance) )
+				node_reward += obs[k] * coeficient
 
 		# float point reward: 1/k * reward on each slice
 		return node_reward/n.max_k
